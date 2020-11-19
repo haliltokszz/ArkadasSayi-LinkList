9ec4efac9e2441773fe249d4c3339a8b06dd9a9c
haliltokszz <toksozhalil@gmail.com>
2020-11-19 14:43:09 +0300

    Proje dosyası ekle.

---
+++ ArkadasSayi-LinkList/LinkedList.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkadasSayi_LinkList
+{
+    public class LinkedList : LinkedListADT
+    {
+        public override void InsertFirst(int value)
+        {
+            Node tmpHead = new Node 
+            { 
+                Data = value 
+            };
+            
+            if (Head == null)
+                Head = tmpHead;
+            else
+            {
+                //En kritik nokta: tmpHead'in next'i eski Head'i göstermeli
+                tmpHead.Next = Head;
+                //Yeni Head artık tmpHead oldu
+                Head = tmpHead;
+            }
+            //Bağlı listedeki eleman sayısı bir arttı
+            Size++;
+        }
+
+        public override void InsertLast(int value)
+        {
+            //Eski sonuncu node, Head'den başlanarak set ediliyor
+            Node oldLast = Head;
+
+            if (Head == null)
+                //Hiç kayıt eklenmediği için InsertFirst çağrılabilir
+                InsertFirst(value);
+            else
+            {
+                //Yeni sonuncu node yaratılıyor
+                Node newLast = new Node 
+                { 
+                    Data = value 
+                };
+
+                //Eski sonuncu node bulunuyor
+                //Tail olsaydı sonuncuyu bulmaya gerek yoktu.
+                while (oldLast.Next != null)
+                {
+                    
+                        oldLast = oldLast.Next;
+                    
+                }
+
+                //Eski sonuncu node referansı artık yeni sonuncu node'u gösteriyor
+                oldLast.Next = newLast;
+
+                //Bağlı listedeki eleman sayısı bir arttı
+                Size++;
+            }
+        }
+
+        public override void InsertPos(int position, int value)
+        {
+            Node eklenecekeleman = new Node
+            {
+                Data = value
+            };
+            if (Head == null)
+            {
+                Head = eklenecekeleman;
+            }
+            else
+            {
+                Node temp = Head;
+                if (position - Size > 1)
+                    Console.WriteLine("Dizi boyutundan buyuk deger girdiniz");
+                else if (position - Size == 1)
+                    InsertLast(value);
+                else if (position == 1)
+                {
+                    InsertFirst(value);
+                    Size--;
+                }
+                    
+                else
+                {
+                    for (int i = 1; i < position - 1; i++)
+                    {
+                        temp = temp.Next;
+                    }
+                    eklenecekeleman.Next = temp.Next;
+                    temp.Next = eklenecekeleman;
+                }
+                Size++;
+            }
+        }
+
+        public override void DeleteFirst()
+        {
+            if (Head != null)
+            {
+                //Head'in next'i HeadNext'e atanıyor
+                Node tempHeadNext = this.Head.Next;
+                //HeadNext null ise zaten tek kayıt olan Head silinir.
+                if (tempHeadNext == null)
+                    Head = null;
+                else
+                    //HeadNext null değilse yeni Head, HeadNext olur.
+                    Head = tempHeadNext;
+                //Listedeki eleman sayısı bir azaltılıyor
+                Size--;
+            }
+        }
+
+        public override void DeleteLast()
+        {
+            //Last node bulunup NULL yapılacak
+            Node lastNode = Head;
+            //Last node'dan bir önceki node'un Next'i null olacak
+            Node lastPrevNode = null;
+            while (lastNode.Next != null)
+            {
+                
+                    lastPrevNode = lastNode;
+                    lastNode = lastNode.Next;                    
+                
+            }
+            //Listedeki eleman sayısı bir azaltılıyor
+            Size--;
+            //Last node null oldu
+            lastNode = null;
+
+            //Last node'dan önceki node varsa onun next'i null olacak yoksa zaten tek kayıt vardır, 
+            //o da Head'dir, o da null olacak
+            if (lastPrevNode != null)
+                lastPrevNode.Next = null;
+            else
+                Head = null;
+        }
+
+        public override void DeletePos(int position)
+        {
+            Node previous = new Node();
+            if (position == 1)
+                DeleteFirst();
+
+            else if (Head == null)
+            {
+                Head = previous;
+            }
+            else
+            {
+                Node temp = Head;
+                if (position > Size)
+                    Console.WriteLine("Dizi boyutundan buyuk deger girdiniz");
+                else
+                {
+                    for (int i = 0; i < position - 1; i++)
+                    {
+                        previous = temp;
+                        temp = temp.Next;
+                    }
+                    previous.Next = temp.Next;
+                }
+                Size--;
+            }
+        }
+
+        public override Node GetElement(int position)
+        {
+            //Geri dönülecek eleman
+            Node retNode = null;
+            //Head'den başlanarak Next node'a gidilecek
+            Node tempNode = Head;            
+            int count = 0;
+
+            while (tempNode != null)
+            {
+                if (count == position-1)
+                {
+                    retNode = tempNode;
+                    break;
+                }
+                //Next node'a git
+                tempNode = tempNode.Next;
+                count++;
+            }
+            return retNode;
+        }
+
+        public override string DisplayElements()
+        {
+            string temp = "";
+            Node item = Head;
+            while (item != null)
+            {
+                temp += "-->" + item.Data;
+                item = item.Next;
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ ArkadasSayi-LinkList/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkadasSayi_LinkList
+{
+    class Program
+    {
+        static LinkedList dizi = new LinkedList();
+
+        public static bool KontrolEtveEkle(int deger)
+        {
+            bool eklenebilirMi = true;
+            if (dizi.Size != 0)
+            {
+                Node veri = dizi.Head;
+                while (veri != null)
+                {
+                    if (veri.Data == deger)
+                    {
+                        Console.WriteLine("Lütfen farklı bir sayı giriniz. Eklemek istediğiniz sayı LinkedList'te bulunmaktadır.");
+                        eklenebilirMi = false;
+                        return false;
+                    }
+                    veri = veri.Next;
+                }
+                if (eklenebilirMi)
+                    dizi.InsertFirst(deger);
+            }
+            else
+                dizi.InsertFirst(deger);
+            return true;
+        }
+        public static void ArkadasSayi(LinkedList dizi) { 
+        //worst-case Big(O) karmaşıklığı = O(n^3) fakat en kötü durumda 4*n+5*n denemede buluyoruz (n= sayı değeri)
+        //fonksiyon öncelikle döngüden sayıların bölenlerinin toplamını buluyor. Ardından dizinin içinde bu toplama eşit bir sayı olup olmadığını kontrol ediyoruz
+        //eğer varsa direk o sayının da bölenlerinin toplamını bulup arkdaş sayı olup olmadıklarını kontrol ediyoruz böylece daha hızlı sonuç almak mümkün oluyor...
+            for (int i=1; i<=dizi.Size; i++)
+            {
+                
+                int headPos = 1;
+                int toplam=0, bolen=1;
+                Node node = dizi.GetElement(i);
+                Node temp = dizi.Head;
+                while (bolen < node.Data) //kendisi hariç bölenleri
+                {
+                    if (node.Data % bolen == 0) //tam bölünüyorsa bölen toplama eklenir
+                        toplam += bolen;
+                    bolen++;
+                }
+                while (temp!=null)//dizi sınırları içinde geziyoruz
+                {
+                    if (temp.Data == toplam)//eğer dizinin içindeki bir sayı bölenlerin toplamına eşitse
+                    {
+                        int toplam2 = 0, bolen2 = 1;
+                        while (bolen2 < temp.Data) //kendisi hariç bölenleri
+                        {
+                            if (temp.Data % bolen2 == 0) //tam bölünüyorsa bölen toplama eklenir
+                                toplam2 += bolen2;
+                            bolen2++;
+                        }
+                        if(toplam2== node.Data)//eğer ikinci sayının bölenler toplamı da birinci sayıya eşitse arkadaş sayılardır 
+                        {
+                            dizi.DeletePos(i);
+                            dizi.DeletePos(headPos-1);
+                            dizi.InsertPos(headPos-1, node.Data);
+                            dizi.InsertPos(i, temp.Data);
+                            return;
+                        }
+                    }
+                    temp = temp.Next; //eger değilse ilerliyoruz ve headPos' u arttırıyoruz...
+                    headPos++;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5 farklı tam sayı içerisindeki 2 arkadaş sayıyı bulur ve yerlerini değiştirir.");
+            Console.WriteLine("Lütfen ekrana 5 farklı tam sayı giriniz...");
+            for(int i=0; i<5; i++)
+            {
+                if (KontrolEtveEkle(Convert.ToInt32(Console.ReadLine())) == false)
+                {
+                    i--;
+                }
+            }
+            Console.WriteLine(dizi.DisplayElements());
+            ArkadasSayi(dizi);
+            Console.WriteLine(dizi.DisplayElements());
+            Console.Read();
+        }
+    }
+}
